2b131a73204a9e14ca3a8c907d3c18161b837d52
OfirDataloopAI <ofir.g@dataloop.ai>
2024-04-15 19:13:52 +0300

    added download from source option

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,23 +1,40 @@
 import dtlpy as dl
+import requests
+import os
+import logging
+
 import custom_converter as lidar
+
+logger = logging.getLogger()
 
 
 class DatasetLidarOSDAR(dl.BaseServiceRunner):
     def __init__(self):
         dl.use_attributes_2(state=True)
 
+        # self.dataset_url = "https://download.data.fid-move.de/dzsf/osdar23/1_calibration_1.2.zip"
+        self.dataset_url = "https://download.data.fid-move.de/dzsf/osdar23/9_station_ruebenkamp_9.7.zip"
+        self.zip_filename = "data.zip"
+
         self.enable_ir_cameras = "false"
         self.enable_rgb_cameras = "false"
         self.enable_rgb_highres_cameras = "true"
 
-    def upload_dataset(self, dataset: dl.Dataset, source: str):
-        # TODO: used download command to get the zip
+    def download_zip(self):
+        # Download the file
+        response = requests.get(url=self.dataset_url)
+        zip_filepath = os.path.join(os.getcwd(), self.zip_filename)
+        with open(zip_filepath, 'wb') as file:
+            file.write(response.content)
 
-        # if "zip" not in item.mimetype:
-        #     raise dl.exceptions.BadRequest(
-        #         status_code="400",
-        #         message="Input Item mimetype isn't zip!"
-        #     )
+        logger.info(msg=f"File downloaded to: {zip_filepath}")
+        return zip_filepath
+
+    def upload_dataset(self, dataset: dl.Dataset, source: str):
+        if self.zip_filename not in os.listdir(path=os.getcwd()):
+            zip_filepath = self.download_zip()
+        else:
+            zip_filepath = os.path.join(os.getcwd(), self.zip_filename)
 
         item: dl.Item
         lidar_parser = lidar.LidarCustomParser(
@@ -29,5 +46,14 @@ class DatasetLidarOSDAR(dl.BaseServiceRunner):
         return frames_item
 
 
-if __name__ == '__main__':
+def test_download():
     sr = DatasetLidarOSDAR()
+    sr.download_zip()
+
+
+def main():
+    test_download()
+
+
+if __name__ == '__main__':
+    main()
