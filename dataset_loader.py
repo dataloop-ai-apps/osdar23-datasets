53ff32ebe4ee8cc02ac48d891040551b38876463
Or Shabtay <or@dataloop.ai>
2024-07-02 09:52:06 +0300

    Initial commit

---
+++ dataset_loader.py
@@ -0,0 +1,98 @@
+import dtlpy as dl
+import requests
+import os
+import logging
+import json
+
+import custom_converter as lidar
+
+logger = logging.getLogger(name='osdar-dataset')
+
+
+class DatasetLidarOSDAR(dl.BaseServiceRunner):
+    def __init__(self):
+        dl.use_attributes_2(state=True)
+
+        # Original sources
+        # self.dataset_url = "https://download.data.fid-move.de/dzsf/osdar23/1_calibration_1.1.zip"  # 10 Frame
+        # self.dataset_url = "https://download.data.fid-move.de/dzsf/osdar23/1_calibration_1.2.zip"  # 100 Frames
+
+        self.dataset_url = "https://storage.googleapis.com/model-mgmt-snapshots/datasets-OSDAR2023/1_calibration_1_1_subset.zip"
+        self.zip_filename = "data.zip"
+        self.ontology_filename = "OSDAR Ontology.json"
+
+        self.enable_ir_cameras = "false"
+        self.enable_rgb_cameras = "false"
+        self.enable_rgb_highres_cameras = "true"
+
+    def _import_recipe_ontology(self, dataset: dl.Dataset):
+        recipe = dataset.recipes.list()[0]
+        ontology = recipe.ontologies.list()[0]
+
+        new_ontology_filepath = os.path.join(os.path.dirname(str(__file__)), self.ontology_filename)
+        with open(file=new_ontology_filepath, mode='r') as file:
+            new_ontology_json = json.load(fp=file)
+
+        new_ontology = dl.Ontology.from_json(_json=new_ontology_json, client_api=dl.client_api, recipe=recipe)
+        new_ontology.id = ontology.id
+        new_ontology.creator = ontology.creator
+        new_ontology.metadata["system"]["projectIds"] = recipe.project_ids
+        new_ontology.update()
+        return recipe
+
+    def _download_zip(self):
+        zip_filepath = os.path.join(os.getcwd(), self.zip_filename)
+        with requests.get(self.dataset_url, stream=True) as r:
+            r.raise_for_status()
+            with open(zip_filepath, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        logger.info(msg=f"File downloaded to: {zip_filepath}")
+        return zip_filepath
+
+    def upload_dataset(self, dataset: dl.Dataset, source: str):
+        self._import_recipe_ontology(dataset=dataset)
+        if self.zip_filename not in os.listdir(path=os.getcwd()):
+            zip_filepath = self._download_zip()
+        else:
+            zip_filepath = os.path.join(os.getcwd(), self.zip_filename)
+
+        item: dl.Item
+        lidar_parser = lidar.LidarCustomParser(
+            enable_ir_cameras=self.enable_ir_cameras,
+            enable_rgb_cameras=self.enable_rgb_cameras,
+            enable_rgb_highres_cameras=self.enable_rgb_highres_cameras
+        )
+        frames_item = lidar_parser.custom_parse_data(zip_filepath=zip_filepath, lidar_dataset=dataset)
+        return frames_item
+
+
+def test_download():
+    sr = DatasetLidarOSDAR()
+    sr._download_zip()
+
+
+def test_import_recipe_ontology():
+    dataset_id = "66325a24241a71f884f78431"
+
+    dataset = dl.datasets.get(dataset_id=dataset_id)
+    sr = DatasetLidarOSDAR()
+    sr._import_recipe_ontology(dataset=dataset)
+
+
+def test_dataset_import():
+    dataset_id = "663b93cfd03cf2f75ddeff4f"
+
+    dataset = dl.datasets.get(dataset_id=dataset_id)
+    sr = DatasetLidarOSDAR()
+    sr.upload_dataset(dataset=dataset, source="")
+
+
+def main():
+    # test_download()
+    # test_import_recipe_ontology()
+    test_dataset_import()
+
+
+if __name__ == '__main__':
+    main()
